cfa8a3ad7ffb43bf65ba4870aa752eac44945f4f
Sand <sandboxasdf@gmail.com>
2024-07-05 16:54:16 +1000

    run into a snag with the click functionality, js encapsulation weirdness

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # odin-tictactoe
 https://www.theodinproject.com/lessons/node-path-javascript-tic-tac-toe
+
+Some ideas lifted from this article:
+https://codebrainer.com/blog/tic-tac-toe-javascript-game
--- script.js
+++ script.js
@@ -7,14 +7,70 @@ const spacePlayerTwo = 2; // o
 
 var currentPlayer = spacePlayerOne;
 
+const WINNING_COMBINATIONS = [
+    // HORIZONTAL
+    [[0, 0], [0, 1], [0, 2]],
+    [[1, 0], [1, 1], [1, 2]],
+    [[2, 0], [2, 1], [2, 2]],
+    // VERTICAL
+    [[0, 0], [1, 0], [2, 0]],
+    [[0, 1], [1, 1], [2, 1]],
+    [[0, 2], [1, 2], [2, 2]],
+];
+
 const gameBoard = (() => {
     // create a 2d array where each cell contains a 0
     // let array = Array(rows).fill().map(() => Array(columns).fill(0));
     this.boardGrid = Array(3).fill().map(() => Array(3).fill(0)); // this is "function scope", ie, private
 
-    const markSpace = (row, column, currentPlayer) => {
+    const MarkSpaceWithPlayer = (row, column, playerNumber) => {
         console.log("marking space");
-        boardGrid[row][column] = currentPlayer;
+        boardGrid[row][column] = playerNumber;
+        HTMLcontroller.MarkSpaceWithPlayer(row, column, playerNumber);
+
+        CheckWinCondition();
+    };
+
+    function CheckWin(player) {
+        WINNING_COMBINATIONS.forEach(function(combo) {
+            if ((boardGrid[combo[0][0]][combo[0][1]] == player)
+            && (boardGrid[combo[1][0]][combo[1][1]] == player)
+            && (boardGrid[combo[2][0]][combo[2][1]] == player)) {
+                console.log("YOU'RE WINNER" + player);
+            }
+        });
+    }
+
+
+
+    function CheckWinCondition() {
+        CheckWin(spacePlayerOne);
+        CheckWin(spacePlayerTwo);
+        /*
+        // HORIZONTAL
+        if ((boardGrid[0][0] == boardGrid[0][1] && boardGrid[0][1] == boardGrid[0][2]) ||
+        (boardGrid[1][0] == boardGrid[1][1] && boardGrid[1][1] == boardGrid[1][2]) ||
+        (boardGrid[2][0] == boardGrid[2][1] && boardGrid[2][1] == boardGrid[2][2])) {
+            console.log("YOU'RE WINNER");
+        }
+        */
+
+        /*
+        if ( (gridSquareDivs[0][0] == gridSquareDivs[0][1] && gridSquareDivs[0][1] == gridSquareDivs[0][2]) ||
+        (gridSquareDivs[1][0] == gridSquareDivs[1][1] && gridSquareDivs[0][1] == gridSquareDivs[1][2]) ||
+        )
+        */
+    }
+
+    const ClickOnSpace = (index) => {
+        console.log(index);
+        /*
+        let y = Math.floor(index / 3);
+        let x = index % 3
+        console.log("x = " + x);
+        console.log("y = " + y);
+        MarkSpaceWithPlayer (x, y, currentPlayer);
+        */
     };
 
     const printBoardState = () => {
@@ -24,8 +80,9 @@ const gameBoard = (() => {
     }
 
     return {
-        markSpace,
-        printBoardState
+        MarkSpaceWithPlayer,
+        printBoardState,
+        ClickOnSpace
     }
 })();
 
@@ -37,16 +94,26 @@ const HTMLcontroller = (() => {
     const InitializeGame = () => {
         // get children, then convert into a 3x3 2D array
         this.newArr = Array.from(gridParentDiv.children);
+        var i = 0;
+        newArr.forEach(function(child) {
+            console.log(i);
+            child.addEventListener("click", () => {
+                gameBoard.ClickOnSpace(i);
+            })
+            i++;
+        });
+
         while(newArr.length) gridSquareDivs.push(newArr.splice(0,3));
         
         PrintDivs();
-
-        MarkSpaceWithPlayer(0, 1, spacePlayerOne);
-        gridSquareDivs[2][1].classList.add("occupied");
     }
 
     const MarkSpaceWithPlayer = (row, column, playerNumber) => {
+        console.log(row + " / " + column + " / " + playerNumber);
         let currentSpace = gridSquareDivs[row][column];
+
+        if (currentSpace == null)
+            console.log("Oh no");
 
         currentSpace.classList.add("occupied");
         switch(playerNumber) {
@@ -57,7 +124,6 @@ const HTMLcontroller = (() => {
                 currentSpace.classList.add("playerO");
                 break;
         }
-
     }
 
     function PrintDivs() {
@@ -75,8 +141,14 @@ const HTMLcontroller = (() => {
 
 HTMLcontroller.InitializeGame();
 
+/*
 gameBoard.printBoardState();
-gameBoard.markSpace(1, 1, spacePlayerOne);
+gameBoard.MarkSpaceWithPlayer(0, 0, spacePlayerOne);
+gameBoard.MarkSpaceWithPlayer(1, 0, spacePlayerTwo);
+gameBoard.MarkSpaceWithPlayer(0, 1, spacePlayerOne);
+gameBoard.MarkSpaceWithPlayer(1, 1, spacePlayerTwo);
+gameBoard.MarkSpaceWithPlayer(0, 2, spacePlayerOne);
 gameBoard.printBoardState();
+*/
 // console.log(game.boardGrid[0][0]); // produces error because boardGrid is private
 
